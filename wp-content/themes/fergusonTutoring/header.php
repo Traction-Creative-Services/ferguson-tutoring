6e0e085b5f4b0f9a17c2f2ec475812eddd2212dd
Martin M Sheeks <martin.sheeks@gmail.com>
2014-08-19 20:17:01 -0500

    update header

--- wp-content/themes/fergusonTutoring/header.php
+++ wp-content/themes/fergusonTutoring/header.php
@@ -32,10 +32,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <?php
-		$pages = wp_list_pages();
-		foreach($pages as $page) {
-			var_dump($page);
-		}
+		wp_list_pages(array('title_li' => ''));
         ?>
       </ul>
     </div><!-- /.navbar-collapse -->
