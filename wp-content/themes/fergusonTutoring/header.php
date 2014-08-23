dee7de0f4f1c1968b61a8bb8f052440e63b6a3bd
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 15:06:22 -0500

    add logo

--- wp-content/themes/fergusonTutoring/header.php
+++ wp-content/themes/fergusonTutoring/header.php
@@ -25,7 +25,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Ferguson Tutoring</a>
+      <a class="navbar-brand" href="#"><img alt="Ferguson Tutoring" src="<?php echo get_theme_root(); ?>/assets/img/logo.gif"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
