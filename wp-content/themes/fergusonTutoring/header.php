fc3fd5a5d1b8b60a4bf75d319314ad40686e2859
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 17:27:14 -0500

    edit header

--- wp-content/themes/fergusonTutoring/header.php
+++ wp-content/themes/fergusonTutoring/header.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-  <div class="container-fluid">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -33,6 +33,9 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => '', 'menu_class' => 'nav navbar-nav' ) ); ?>
+    <ul class="nav navbar-nav navbar-left">
+      <li>(612) 520-1446</li>
+    </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
