b51cf18da61dcb9ed85cb5360cfc0b12aa03207a
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 16:30:45 -0500

    Add images to home page

--- wp-content/themes/fergusonTutoring/index.php
+++ wp-content/themes/fergusonTutoring/index.php
@@ -11,7 +11,7 @@
 		</div>
 		<div class="row">
 			<div class="col-md-4">
-				<img class="home-feature-img" alt="Mathematics" src="<?php echo get_template_directory_uri(); ?>/assets/img/mathematics.jpg">
+				<img class="home-feature-img" alt="Mathematics" src="<?php echo get_template_directory_uri(); ?>/assets/img/maths.jpg">
 				<h3>MATHEMATICS</h3>
 				<ul>
 					<li>Algebra</li>
@@ -23,7 +23,7 @@
 				</ul>
 			</div>
 			<div class="col-md-4">
-				<img class="home-feature-img" alt="Science and English" src="<?php echo get_template_directory_uri(); ?>/assets/img/scienceEnglish.jpg">
+				<img class="home-feature-img" alt="Science and English" src="<?php echo get_template_directory_uri(); ?>/assets/img/microscope.jpg">
 				<h3>SCIENCE &#149; ENGLISH</h3>
 				<ul>
 					<li>AP Biology</li>
@@ -34,7 +34,7 @@
 				</ul>
 			</div>
 			<div class="col-md-4">
-				<img class="home-feature-img" alt="Test Preperation" src="<?php echo get_template_directory_uri(); ?>/assets/img/testPrep.jpg">
+				<img class="home-feature-img" alt="Test Preperation" src="<?php echo get_template_directory_uri(); ?>/assets/img/college.jpg">
 				<h3>TEST PREP</h3>
 				<ul>
 					<li>ACT</li>
