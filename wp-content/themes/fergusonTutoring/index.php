7f791ce02f28ed6f357a6f4ebcf51dfa1755a127
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 15:46:37 -0500

    fill in home page

--- wp-content/themes/fergusonTutoring/index.php
+++ wp-content/themes/fergusonTutoring/index.php
@@ -1,3 +1,53 @@
 <?php get_header(); ?>
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7 col-offset-md-2">
+				<h1>FERGUSON TUTORING</h1>
+				<h2>Educating the Twin Cities Area</h2>
+			</div>
+			<div class="col-md-3">
+				<!-- PHP for news headlines -->
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<img class="home-feature-img" alt="Mathematics" src="<?php echo get_template_directory_uri(); ?>/assets/img/mathematics.jpg">
+				<h3>MATHEMATICS</h3>
+				<ul>
+					<li>Algebra</li>
+					<li>Geometry</li>
+					<li>Trigonometry</li>
+					<li>Precalculus</li>
+					<li>AP Calculus</li>
+					<li>AP Statistics</li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<img class="home-feature-img" alt="Science and English" src="<?php echo get_template_directory_uri(); ?>/assets/img/scienceEnglish.jpg">
+				<h3>SCIENCE &#149; ENGLISH</h3>
+				<ul>
+					<li>AP Biology</li>
+					<li>AP Chemistry</li>
+					<li>AP Physics</li>
+					<li>AP English Language</li>
+					<li>AP English Literature</li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<img class="home-feature-img" alt="Test Preperation" src="<?php echo get_template_directory_uri(); ?>/assets/img/testPrep.jpg">
+				<h3>TEST PREP</h3>
+				<ul>
+					<li>ACT</li>
+					<li>SAT</li>
+					<li>Advanced Placement (AP)</li>
+					<li>International Baccalaureate (IB)</li>
+					<li>GRE</li>
+					<li>GMAT</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row" id="testimonials-bar">
+			<!-- testimonials code -->
+		</div>
+	</div>
 <?php get_footer(); ?>
