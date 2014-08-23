3bb761ae5224accf6a747e0013e2e8c6e0550d4f
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 16:53:39 -0500

    layer home

--- wp-content/themes/fergusonTutoring/index.php
+++ wp-content/themes/fergusonTutoring/index.php
@@ -9,43 +9,49 @@
 				<!-- PHP for news headlines -->
 			</div>
 		</div>
-		<div class="row" id="features">
-			<div class="col-md-4">
-				<img class="home-feature-img" alt="Mathematics" src="<?php echo get_template_directory_uri(); ?>/assets/img/maths.jpg">
-				<h3>MATHEMATICS</h3>
-				<ul>
-					<li>Algebra</li>
-					<li>Geometry</li>
-					<li>Trigonometry</li>
-					<li>Precalculus</li>
-					<li>AP Calculus</li>
-					<li>AP Statistics</li>
-				</ul>
-			</div>
-			<div class="col-md-4">
-				<img class="home-feature-img" alt="Science and English" src="<?php echo get_template_directory_uri(); ?>/assets/img/microscope.jpg">
-				<h3>SCIENCE &#149; ENGLISH</h3>
-				<ul>
-					<li>AP Biology</li>
-					<li>AP Chemistry</li>
-					<li>AP Physics</li>
-					<li>AP English Language</li>
-					<li>AP English Literature</li>
-				</ul>
-			</div>
-			<div class="col-md-4">
-				<img class="home-feature-img" alt="Test Preperation" src="<?php echo get_template_directory_uri(); ?>/assets/img/college.jpg">
-				<h3>TEST PREP</h3>
-				<ul>
-					<li>ACT</li>
-					<li>SAT</li>
-					<li>Advanced Placement (AP)</li>
-					<li>International Baccalaureate (IB)</li>
-					<li>GRE</li>
-					<li>GMAT</li>
-				</ul>
+	</div>
+	<div id="features">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<img class="home-feature-img" alt="Mathematics" src="<?php echo get_template_directory_uri(); ?>/assets/img/maths.jpg">
+					<h3>MATHEMATICS</h3>
+					<ul>
+						<li>Algebra</li>
+						<li>Geometry</li>
+						<li>Trigonometry</li>
+						<li>Precalculus</li>
+						<li>AP Calculus</li>
+						<li>AP Statistics</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<img class="home-feature-img" alt="Science and English" src="<?php echo get_template_directory_uri(); ?>/assets/img/microscope.jpg">
+					<h3>SCIENCE &#149; ENGLISH</h3>
+					<ul>
+						<li>AP Biology</li>
+						<li>AP Chemistry</li>
+						<li>AP Physics</li>
+						<li>AP English Language</li>
+						<li>AP English Literature</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<img class="home-feature-img" alt="Test Preperation" src="<?php echo get_template_directory_uri(); ?>/assets/img/college.jpg">
+					<h3>TEST PREP</h3>
+					<ul>
+						<li>ACT</li>
+						<li>SAT</li>
+						<li>Advanced Placement (AP)</li>
+						<li>International Baccalaureate (IB)</li>
+						<li>GRE</li>
+						<li>GMAT</li>
+					</ul>
+				</div>
 			</div>
 		</div>
+	</div>
+	<div class="container">
 		<div class="row" id="testimonials-bar">
 			<!-- testimonials code -->
 		</div>
