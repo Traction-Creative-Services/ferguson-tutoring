a6c55035fd932499ccd9bdcdf9b9a936916d2f9a
Martin Sheeks <martin.sheeks@gmail.com>
2014-08-23 20:50:05 -0500

    add page template

---
+++ wp-content/themes/fergusonTutoring/page.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Page
+*/
+
+<?php get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-md-offset-2" id="header-text">
+				<h1>FERGUSON TUTORING</h1>
+				<h2>Educating the Twin Cities Area</h2>
+			</div>
+			<div class="col-md-5">
+				<!-- PHP for news headlines -->
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<?php the_content(); ?>
+			</div>
+			<div class="col-md-4">
+				<?php the_field('sidebar'); ?>
+			</div>
+		</div>
+	</div>
+		<?php endwhile; // end of the loop. ?>
+<?php get_footer(); ?>
